e2ce7d9fbb4b575cb44f40590712d394a8b603be
younghee <hn05035@naver.com>
2023-11-30 12:34:48 +0900

    vue-test

---
+++ vue/vue-test/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <VueComponent/>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+import VueComponent from './components/VueComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //HelloWorld,
+    VueComponent    //하위 컴포넌트(여기에 등록하고 템플릿에 태그 형태로 등록 가능)
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue/vue-test/src/components/VueComponent.vue
@@ -0,0 +1,22 @@
+<template>  
+    <div>
+        <label><input type="text" v-model="msg"></label>        <!--input에서 입력한 값을 p로 넘겨 바로 보여짐-->
+        <p>{{msg}}</p>      <!--{{}}출력하는 것-->
+    </div>
+</template>
+<script>
+    export default {
+        name : 'VueComponent',
+        data(){
+            return {
+                msg : '안녕'
+            }
+        }
+    }
+</script>
+<style>
+    p{
+        color:white;
+        background-color:black;
+    }
+</style>
